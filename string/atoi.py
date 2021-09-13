cf707d9783c403e55d99ff4acec5750f42c94a56
xavifeds8 <65709700+xavifeds8@users.noreply.github.com>
2021-09-13 23:59:17 +0530

    Update atoi.py

--- string/atoi.py
+++ string/atoi.py
@@ -1,18 +1,16 @@
-def atoi(string):
-    # Code here
-    def atoi(n):
-        if n.isdigit():
-            return ord(n) - 48
-        else:
-            return -1
-    res = 0
-    for i in string:
-        if i == "-":
-            continue
-            
-        val = atoi(i)
-        if val == -1:
-            return -1
-        res = res*10+val
-    return res
-print(atoi("12345asdfi"))
+string = "1234"
+def (n):
+    if n.isdigit():
+        return ord(n) - 48
+    else:
+        return -1
+res = 0
+for i in string:
+    if i == "-":
+        continue
+
+    val = atoi(i)
+    if val == -1:
+        return -1
+    res = res*10+val
+print(res)
