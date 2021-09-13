e42de3da4b95e96c8a47547f564f5cbf9b15fab4
xavifeds8 <65709700+xavifeds8@users.noreply.github.com>
2021-09-14 00:00:21 +0530

    Update atoi.py

--- string/atoi.py
+++ string/atoi.py
@@ -1,4 +1,5 @@
 string = "1234"
+flag = 1
 def (n):
     if n.isdigit():
         return ord(n) - 48
@@ -7,10 +8,11 @@ def (n):
 res = 0
 for i in string:
     if i == "-":
+        flag = -1
         continue
 
     val = atoi(i)
     if val == -1:
         return -1
     res = res*10+val
-print(res)
+print(res*flag)
